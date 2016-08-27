cac9d8a4377b391e27d0a928754158122ec34227
iMac <imac@imacdeimac.local>
2016-08-27 17:33:59 +0800

    第三次

---
+++ M-07/Person.h
@@ -0,0 +1,14 @@
+//
+//  Person.h
+//  M-07
+//
+//  Created by iMac on 16/8/27.
+//  Copyright © 2016年 iMac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (nonatomic,assign) int age;
+@end
---
+++ M-07/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  M-07
+//
+//  Created by iMac on 16/8/27.
+//  Copyright © 2016年 iMac. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
